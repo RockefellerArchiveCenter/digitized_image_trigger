d2244918aece29a423dc9f22290ae8b1d09762a1
Hillel Arnold <hillel.arnold@gmail.com>
2025-09-29 13:07:18 -0400

    use print

--- src/handle_digitized_image_trigger.py
+++ src/handle_digitized_image_trigger.py
@@ -113,7 +113,7 @@ def run_task(
                 }
             }
         ]
-    logging.info(task_definition, overrides, volume_configuration)
+    print(task_definition, overrides, volume_configuration)
     response = ecs_client.run_task(
         cluster=config['ECS_CLUSTER'],
         launchType='FARGATE',
